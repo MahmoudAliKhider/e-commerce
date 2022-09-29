202a01026aee520875d1b8f30e07123b6752ea5c
mahmoudAliKhider <mahmoudkhider37@gmail.com>
2022-09-29 16:41:17 +0200

    register

--- routers/auth.js
+++ routers/auth.js
@@ -1,21 +1,25 @@
-const express = require('express')
-const router = express.Router();
+const router = require('express').Router()
+
 const UserModels = require('../models/users');
-const bcrept = require('bcrypt')
-router.post('/register',async(req,res)=>{
+const bcrypt = require('bcrypt')
+
+
+router.post('/register', async (req,res)=>{
     try{
-      const salt = await bcrept.genSalt(10);
-      const hashpass = await bcrept.hash(req.body.password,salt);
+      const salt = await bcrypt.genSalt(10);
+      const hashpass = await bcrypt.hash(req.body.password,salt);
         const newUser= new UserModels({
           username:req.body.username,
           email:req.body.email,
-          password:req.body.password
-        })
+          password: hashpass,
+        });
         const user = await newUser.save();
-        res.status(200).send(user)
+        console.log(user)
+        res.status(200).json(user)
 
     }catch(err){
     res.status(401).json(err)
+    console.log(err)
     }
 })
 
