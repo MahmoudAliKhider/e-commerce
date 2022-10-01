3f96d3977d2b758686f576f0843e50bb26d84e95
mahmoudAliKhider <mahmoudkhider37@gmail.com>
2022-10-01 17:15:36 +0200

    cart

--- routers/cart.js
+++ routers/cart.js
@@ -19,7 +19,7 @@ router.get('/',async(req,res)=>{
     const Carts = await cartModels.find();
   
     try {
-      res.status(200).json(orders)
+      res.status(200).json(Carts)
       
     } catch (err) {
       res.status(500).json(err)
